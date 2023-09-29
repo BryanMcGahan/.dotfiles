80b3410795bd30c387110c03fdc1de537325135a
BryanMcGahan <mcgahan72@outlook.com>
2023-09-29 09:35:48 -0500

    change theme

--- nvim/.config/nvim/lua/custom/plugins/rosepine.lua
+++ nvim/.config/nvim/lua/custom/plugins/rosepine.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         require("rose-pine").setup({
             variant = 'auto',
-            dark_variant = 'moon',
+            dark_variant = 'main',
             dim_nc_background = true,
             disable_background = false,
             bold_vert_split = false,
