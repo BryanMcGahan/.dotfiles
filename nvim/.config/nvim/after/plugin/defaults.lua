93f86a661269bcbba439a28946ac114de04e2661
BryanMcGahan <mcgahan72@outlook.com>
2023-11-21 12:26:08 -0600

    I truly don't remember

--- nvim/.config/nvim/after/plugin/defaults.lua
+++ nvim/.config/nvim/after/plugin/defaults.lua
@@ -30,4 +30,10 @@ vim.opt.shiftwidth = 4
 
 vim.opt.termguicolors = true
 
-vim.cmd("colorscheme rose-pine")
+-- vim.g.nord_contrast = true
+-- vim.g.nord_borders = true
+-- vim.g.nord_bold = true
+--
+-- require('nord').set()
+
+vim.cmd("colorscheme nord")
