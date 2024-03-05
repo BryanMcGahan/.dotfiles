cd452f4bbe71a302480a94f95d5c7a762cbe69a8
BryanMcGahan <mcgahan72@outlook.com>
2024-03-04 22:21:06 -0600

    i dont know

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,16 +7,14 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = 'nord'
 -- config.color_scheme = 'One Dark (Gogh)'
 -- config.color_scheme = 'Kanagawa (Gogh)'
--- config.color_scheme = 'nightfox'
+-- config.color_scheme = 'Moonfly (Gogh)'
+config.color_scheme = "nightfox"
 -- config.color_scheme = 'duskfox'
 -- config.color_scheme = 'nordfox'
 -- config.color_scheme = 'terafox'
@@ -24,25 +22,31 @@ end
 -- config.color_scheme = 'Flexoki (Dark)'
 -- config.color_scheme = 'OneDark (base16)'
 -- config.color_scheme = 'rose-pine'
-config.color_scheme = 'rose-pine-moon'
--- config.color_scheme = 'dragon'
--- config.color_scheme = 'nordic'
+-- config.color_scheme = 'rose-pine-moon'
+-- config.color_scheme = "nordic"
+-- config.color_scheme = "Tokyo Night Moon"
 
-config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 
 local font_size
 local hostname = wezterm.hostname()
 if hostname == "GigaLaptop.home" or hostname == "GigaLaptop.local" then
-  font_size = 14
+	font_size = 14
 else
-  font_size = 12
+	font_size = 12
 end
 
 config.font_size = font_size
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+-- config.window_background_image = "/Users/bmcgahan/Pictures/appa.jpg"
+-- config.window_background_image_hsb = {
+-- 	brightness = 0.5,
+-- }
+
+-- config.window_background_opacity = 0.95
 
 -- and finally, return the configuration to wezterm
 return config
-
-
