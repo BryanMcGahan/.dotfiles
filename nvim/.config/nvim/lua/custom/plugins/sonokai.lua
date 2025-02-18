452d1edd0ec0262a4f1622eac029574368864a33
Bryan McGahan <mcgahan72@outlook.com>
2025-02-18 07:24:42 -0600

    removing alot

--- nvim/.config/nvim/lua/custom/plugins/sonokai.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'sainnhe/sonokai',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-    end
-}
